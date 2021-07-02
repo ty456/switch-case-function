c4c0944166e4cbda3b907026ed28b589210c24f9
shovan <rchpurnell.20@gmail.com>
2021-07-02 19:02:01 +0530

    switct

---
+++ 1.js
@@ -0,0 +1,49 @@
+function caseInSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+    switch (val) {
+      case 1:
+        return "alpha";
+        break;
+      case 2:
+        return "beta";
+        break;
+      case 3:
+        return "gamma";
+        break;
+      case 4:
+        return "delta";
+        break;
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  // Change this value to test
+  console.log( caseInSwitch(1));
+
+
+  //solution 2
+  function caseInSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+    switch (val) {
+      case 1:
+        answer = "alpha";
+        break;
+      case 2:
+        answer = "beta";
+        break;
+      case 3:
+        answer = "gamma";
+        break;
+      case 4:
+        answer = "delta";
+        break;
+    }
+    // Only change code above this line
+    return answer;
+  }
+  // Change this value to test
+   console.log( caseInSwitch(1));
